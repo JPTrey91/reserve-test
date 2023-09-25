4e555736faa2fee2e5d0e0387bf400f4ff9895f2
Jon Paul Simonelli <jonpaul.simonelli@gmail.com>
2023-09-25 13:04:29 -0400

    Remove unused src-based image directory in place of public solution.

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../about.css";
 
-import Rachel from "../Images/teamPics/RACHELMCCREADY.jpeg";
+import Rachel from "/assets/images/teamPics/RACHELMCCREADY.jpeg";
 
 import TeamBuilder from "../components/TeamBuilder";
 
